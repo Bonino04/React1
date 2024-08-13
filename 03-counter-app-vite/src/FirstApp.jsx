23a59bd6c249cb03072d5d67718aeab503caacff
Bonino04 <gonza.bonino.2004@gmail.com>
2024-08-13 09:59:02 -0300

    nuevo

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,23 +1,47 @@
 import { Link } from "react-router-dom"
-
+import { useState } from "react"
 
 export const FirstApp = () => {
+
+  const [nombre, setNombre] = useState('')
+  const [apellido, setApellido] = useState('')
+  const [email, setEmail] = useState('')
+  const [contraseña, setContraseña] = useState('')
+
+
+  const handleSubmit = (e) => {
+      e.preventDefault()
+      const data = {
+         nombre: nombre,
+        apellido: apellido,
+        email: email,
+        contraseña: contraseña
+
+      }
+    try {
+       console.log(data)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div>
     <div className="text-center  bg-gradient-to-r from-purple-500 via-pink-500 to-red-500   mb-6 flex">
     <h1 className='m-auto'>Registrar Usuario</h1>
     <h2 className=''>Usuario</h2>
     </div>
-    <div id="Formulario" className='w-6/12 m-auto flex bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 w-96'>
-      <form className='flex flex-col  p-4 m-auto text-black'>
+    <div id="Formulario" className=' m-auto flex bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 w-96'>
+      <form onSubmit={handleSubmit} className='flex flex-col  p-4 m-auto text-black'>
         <label className="ml-5 mb-2 text-black font-bold" >Nombre</label>
-        <input type='text' id='nombre' name='nombre' className='px-2 py-1 rounded-md ml-5  ' placeholder='Nombre' required></input>
+        <input type='text' id='nombre' name='nombre' value={nombre} onChange={(e) => setNombre(e.target.value)} className='px-2 py-1 rounded-md ml-5  ' placeholder='Nombre' ></input>
         <label className="ml-5 mb-2 mt-4   text-black font-bold" >Apellido</label>
-        <input type='text' id='apellido' name='apellido' className='px-2 py-1 rounded-md ml-5 ' placeholder='Apellido' required></input>
+        <input type='text' id='apellido' name='apellido'value={apellido} onChange={(e) => setApellido(e.target.value)} className='px-2 py-1 rounded-md ml-5 ' placeholder='Apellido' ></input>
         <label className="ml-5 mb-2 mt-4  text-black font-bold" >Email</label>
-        <input type='email' id='email' name='email ' className='ml-5 px-2 py-1 rounded-md' placeholder='Email' required></input>
+        <input type='email' id='email' name='email ' value={email} onChange={(e) => setEmail(e.target.value)} className='ml-5 px-2 py-1 rounded-md' placeholder='Email' ></input>
         <label className="ml-5 mb-2 mt-4   text-black font-bold px-2 py-1 rounded-md" >Contraseña</label>
-        <input type='password' id='contraseña' name='contraseña' className='ml-5 px-2 py-1 rounded-md ' placeholder='Password' required></input>
+        <input type='password' id='contraseña' name='contraseña'value={contraseña} onChange={(e) => setContraseña(e.target.value)} className='ml-5 px-2 py-1 rounded-md ' placeholder='Password' ></input>
         <button className="mt-4 w-36 m-auto bg-white  font-bold rounded-md" type="submit">Confirmar</button>
       
         </form>
--- 03-counter-app-vite/src/HelloWorldApp.jsx
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -3,20 +3,18 @@ import { Link } from "react-router-dom"
 
 export const HelloWorldApp = () => {
   return (
+    
     <div>
+      
     <div className="text-center  bg-gradient-to-r from-purple-500 via-pink-500 to-red-500   mb-6 flex">
     <h1 className='m-auto'>Gonza Noticias</h1>
     <h2 className=''>Usuario</h2>
     </div>
-    <div className="stat">
-     <div className="stat-title">Personas que vieron la pagina</div>
-     <div className="stat-value">89,400</div>
-     <div className="stat-desc">21% more than last month</div>
-   </div>
     <div>
         <p className='text-center '>Volver a <button className='hover:bg-purple-600 rounded-md'> <Link to='/'>Inicio</Link></button></p>
     </div>
     </div>
+    
     
 )
 }
